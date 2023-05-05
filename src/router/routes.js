1a04a552a1f5de0bfd74b39cee959d928aa1b4f3
YXJR <846615152@qq.com>
2023-05-05 11:32:40 +0800

    fixed-路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -26,31 +26,32 @@ const routes = [
   },
 
   {
-    path: "/compsitionApi",
+    path: "/family",
+    name: "family",
     meta: {
-      title: "composition API",
-      hasChild: true,
+      title: "全家桶相关",
+      // hasChild: true,
     },
-    component: () => import("../views/CompositionApi.vue"),
-    children: [
-      {
-        path: "setup",
-        name: "setup",
-        meta: {
-          title: "setup",
-        },
-        component: () => import("../components/Setup.vue"),
-      },
+    component: () => import("../views/Family.vue"),
+    // children: [
+    //   {
+    //     path: "VueRouter",
+    //     name: "setup",
+    //     meta: {
+    //       title: "setup",
+    //     },
+    //     component: () => import("../components/Setup.vue"),
+    //   },
 
-      {
-        path: "lifecycle",
-        name: "lifecycle",
-        meta: {
-          title: "生命周期",
-        },
-        component: () => import("../components/LifeCycles.vue"),
-      },
-    ],
+    //   {
+    //     path: "lifecycle",
+    //     name: "lifecycle",
+    //     meta: {
+    //       title: "生命周期",
+    //     },
+    //     component: () => import("../components/LifeCycles.vue"),
+    //   },
+    // ],
   },
 ]
 export default routes
